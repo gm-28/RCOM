594211f39ee2b32d31cbcf14a3af71e2cd95694a
Armazens <94800661+Armazens@users.noreply.github.com>
2021-11-29 15:26:14 +0000

    Added I & RR frames

--- Lab1/defines.h
+++ Lab1/defines.h
@@ -17,17 +17,25 @@
 //XOR(A,C) -> 2
 #define BCC2 (A^C2)
 
-//0bX -> X I TX
-#define C_I 0X00
-//XOR(A,C) -> X
-#define BCC_I (A^C_I)
+//0b00000000 -> 0 I TX
+#define C_I0 0X00
+//XOR(A,C_I0) -> 5
+#define BCC_I0 (A^C_I0)
+//0b00000010 -> 2 I TX
+#define C_I1 0X02
+//XOR(A,C_I1) -> 7
+#define BCC_I1 (A^C_I1)
 
-//0bX -> X RR RX
-#define C_RR 0X00
-//XOR(A,C) -> X
-#define BCC_RR (A^C_RR)
+//0b00000001 -> 1 RR RX
+#define C_RR0 0X01
+//XOR(A,C_RR0) -> 4
+#define BCC_RR0 (A^C_RR0)
+//0b00100001 -> 21 RR RX
+#define C_RR1 0X21
+//XOR(A,C_RR1) -> 24
+#define BCC_RR1 (A^C_RR1)
 
-//cDISC
+//0b00001011 -> B DISC
 #define C_DISC 0X0B
-
+//XOR(A,C_DISC) -> E
 #define BCC_DISC (A^C_DISC)
